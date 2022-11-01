1c1a9db7191d4232e88fb1fb68269dc56a17ac7c
sepideh maghami <sepidehmaghamiit@gmail.com>
2022-11-01 20:53:20 +0330

    login page

--- src/components/ShowHidePass/showHidePass.js
+++ src/components/ShowHidePass/showHidePass.js
@@ -1,15 +1,20 @@
 import React,{useState} from "react";
 import './showHidePass.css'
 import { AiFillEye,AiFillEyeInvisible } from "react-icons/ai";
-export default function ShowHidePassword(props) {
+export default function ShowHidePassword() {
     const [isVisible, setVisible] = useState(false);
     const toggle = () => {
       setVisible(!isVisible);
     };
-  
+    
+    const [pass, setPass] = useState();
+    const password = (e) => {
+      setPass(e.target.value)
+    }
+
     return (
       <div className="form-group">
-        <input type={!isVisible ? "password" : "text"} placeholder='Enter your Password...' value={props.value} required />
+        <input type={!isVisible ? "password" : "text"} placeholder='Enter your Password...' onChange={password}/>
         <span className="icon" onClick={toggle}>
           {isVisible ? <AiFillEye/> : <AiFillEyeInvisible/>}
         </span>
--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,20 +1,15 @@
-import React , {useState} from "react";
 import './login.css'
 import ShowHidePassword from '../../components/ShowHidePass/showHidePass'
-function Login (){
-  const [pass, setPass] = useState();
-  const password = (e) => {
-    setPass(e.target.value)
-  }
-
+function Login (props){
+ 
   return (
     <section className='login'>
         <div className='cart'>
             <h1>Hello!</h1>
             <h3>Sign in your account</h3>
             <form className='login-form'>
-                <input type="text" placeholder='Enter your username...' onChange={password}/>
-                <ShowHidePassword value={pass}/>
+                <input type="text" placeholder='Enter your username...'/>
+                <ShowHidePassword value={props.pass}/>
                 <button type="submit">Login</button>
             </form>
             <p><a href='#'>Create Account</a></p>
