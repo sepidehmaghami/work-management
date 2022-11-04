6de082359c5fa60404f87d7445316136a671a824
sepideh maghami <sepidehmaghamiit@gmail.com>
2022-11-04 14:34:28 +0330

    add home page

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import Header from '../../components/Header/header';
+import ToDoList from '../../assets/toDoList.png';
+import Timer from '../../assets/timer.png';
+import Stopwatch from '../../assets/stopwatch.png';
+import './home.css';
+
 const Home = () => {
   return (
     <div>
       <Header/>
+      <section className='home'>
+        
+        <div className='home-cart'>
+          <a href='#' className='toDoList'>
+            <img src={ToDoList} alt="ToDoList"/>
+            <p>To Do List</p>
+          </a>
+        </div>
+
+        <div className='home-cart'>
+          <a href='#'>
+            <img src={Timer} alt="Timer"/>
+            <p>Timer</p>
+          </a>
+        </div>
+
+        <div className='home-cart'>
+          <a href='#'>
+            <img src={Stopwatch} alt="Stopwatch"/>
+            <p>Stopwatch</p>
+          </a>
+        </div>
+
+      </section>
     </div>
   )
 }
